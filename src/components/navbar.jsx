894161ee86c9e99b02d921dcbf9b811bb812bcb7
Rajveer Shaw <rajveershaw2003@gmail.com>
2024-05-16 17:29:43 +0530

    update responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,11 +6,11 @@ function Navbar(){
     <div className={style.navbar}>
         <div className={style.rnp_logo}></div>
         <div className={style.contact_info}>
-            <div className={style.contact_info_content}>Products</div>
-            <div className={style.contact_info_content}>Our Services</div>
-            <div className={style.contact_info_content}>Events</div>
-            <div className={style.contact_info_content}>Our Teams</div>
-            <div className={style.contact_info_content}>Careers</div>
+            <div className={style.contact_info_content}><p>Products</p></div>
+            <div className={style.contact_info_content}><p>Our Services</p></div>
+            <div className={style.contact_info_content}><p>Events</p></div>
+            <div className={style.contact_info_content}><p>Our Teams</p></div>
+            <div className={style.contact_info_content}><p>Careers</p></div>
             <button className={style.contact_info_button}>Contact us</button>
         </div>
     </div>
