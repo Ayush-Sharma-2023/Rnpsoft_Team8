e293d52be8338fb9f9dee3ac1fff3ef1382f1f6a
Ayush-Sharma-2023 <2022007449.ayush@ug.sharda.ac.in>
2024-05-16 16:09:39 +0530

    modified:   src/App.jsx 	new file:   src/components/body_2.jsx 	new file:   src/components/body_2.module.css 	new file:   src/components/body_3.jsx 	new file:   src/components/body_3.module.css 	new file:   src/components/body_4.jsx 	new file:   src/components/body_4.module.css 	new file:   src/images/background_img2.png 	new file:   src/images/video.png

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from "./components/navbar.jsx"
 import Body_1 from "./components/body_1.jsx"
+import Body_2 from './components/body_2.jsx'
+import Body_3 from './components/body_3.jsx'
 
 function App() {
   return(<>
     <Navbar />
     <Body_1 />
+    <Body_2 />
+    <Body_3 />
+
   </>)
 }
 
---
+++ src/components/body_3.jsx
@@ -0,0 +1,25 @@
+import style from "./body_3.module.css"
+
+
+function Body_3() {
+    return (
+        <>
+            <div className={style.backgr}>
+            <br/><br/>
+            <div className={style.Demo}>Watch a demo</div>
+            <br/>
+            <div className={style.info}>
+                At RnPsoft, we pride ourselves on our innovative solutions ,<br />
+                bfehhirier, erhehrereuryeuoy ehoahoo hoiwri  iuierierrer
+            </div>
+            <br/><br/>
+            <div className={style.video}></div>
+            
+                
+                
+            </div>
+        </>
+    )
+}
+
+export default Body_3;
