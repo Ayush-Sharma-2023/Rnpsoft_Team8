e32f48023ee39a116cc09ae6aa4213fb141f7ada
Ayush-Sharma-2023 <2022007449.ayush@ug.sharda.ac.in>
2024-05-16 13:53:54 +0530

    modified:   Ms-kalinga-ai/vite.config.js

--- Ms-kalinga-ai/vite.config.js
+++ Ms-kalinga-ai/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ms-kalinga-ai"
+  base: "/Rnpsoft_Team/Ms-kalinga-ai"
 })
