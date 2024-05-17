3079074f16a6edc9da2a86e3613d8f491ef7257c
Ayush-Sharma-2023 <2022007449.ayush@ug.sharda.ac.in>
2024-05-17 15:17:55 +0530

    used prettier to make code look better

--- src/components/body_2.jsx
+++ src/components/body_2.jsx
@@ -1,58 +1,67 @@
-import style from "./body_2.module.css"
-
+import style from "./body_2.module.css";
 
 function Body_2() {
-    return (
-        <>
-            
-            <div className={style.backgr}>               
-            
-                
-                <div className={style.Features}>Features</div>
-                <br/><br/><br/><br/>
+  return (
+    <>
+      <div className={style.backgr}>
+        <div className={style.Features}>Features</div>
+        <br />
+        <br />
+        <br />
+        <br />
 
-                <div className={style.Features_Container}>
-
-                    <div className={style.box}>
-                        <div className={style.title}> For Students</div>
-                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
-                    </div>
-
-                    <div className={style.box}>
-                        <div className={style.title}>For Researchers</div>
-                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
-                    </div>
-
-                    <div className={style.box}>
-                        <div className={style.title}>For Professionals</div>
-                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
-                    </div>
-                    
-
-                    <div className={style.box}>
-                        <div className={style.title}>Multi-File Chats</div>
-                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
-                    </div>
-
-                    <div className={style.box}>
-                        <div className={style.title}>Cited Sources</div>
-                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
-                    </div>
-
-                    <div className={style.box}>
-                        <div className={style.title}>Any Language</div>
-                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
-                    </div>
-                
-                
-
-                </div>
-
-                
-                
+        <div className={style.Features_Container}>
+          <div className={style.box}>
+            <div className={style.title}> For Students</div>
+            <div className={style.info}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              libero atque rem commodi, pariatur voluptatum.
             </div>
-        </>
-    )
+          </div>
+
+          <div className={style.box}>
+            <div className={style.title}>For Researchers</div>
+            <div className={style.info}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              libero atque rem commodi, pariatur voluptatum.
+            </div>
+          </div>
+
+          <div className={style.box}>
+            <div className={style.title}>For Professionals</div>
+            <div className={style.info}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              libero atque rem commodi, pariatur voluptatum.
+            </div>
+          </div>
+
+          <div className={style.box}>
+            <div className={style.title}>Multi-File Chats</div>
+            <div className={style.info}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              libero atque rem commodi, pariatur voluptatum.
+            </div>
+          </div>
+
+          <div className={style.box}>
+            <div className={style.title}>Cited Sources</div>
+            <div className={style.info}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              libero atque rem commodi, pariatur voluptatum.
+            </div>
+          </div>
+
+          <div className={style.box}>
+            <div className={style.title}>Any Language</div>
+            <div className={style.info}>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
+              libero atque rem commodi, pariatur voluptatum.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Body_2;
--- src/components/body_3.jsx
+++ src/components/body_3.jsx
@@ -1,35 +1,30 @@
-import style from "./body_3.module.css"
-import kalingaVideo from '../images/kalinga_ai.mp4';
-
+import style from "./body_3.module.css";
+import kalingaVideo from "../images/kalinga_ai.mp4";
 
 function Body_3() {
-    return (
-        <>
-            <div className={style.backgr}>
-            <br/><br/>
-            <div className={style.Demo}>Watch a demo</div>
-            <br/>
-            <div className={style.info}>
-                At RnPsoft, we pride ourselves on our innovative solutions ,<br />
-                bfehhirier, erhehrereuryeuoy ehoahoo hoiwri  iuierierrer
-            </div>
-            <br/><br/>
+  return (
+    <>
+      <div className={style.backgr}>
+        <br />
+        <br />
+        <div className={style.Demo}>Watch a demo</div>
+        <br />
+        <div className={style.info}>
+          At RnPsoft, we pride ourselves on our innovative solutions ,<br />
+          bfehhirier, erhehrereuryeuoy ehoahoo hoiwri iuierierrer
+        </div>
+        <br />
+        <br />
 
-           
-
-            <div className={style.video_container}>
-                <video autoPlay loop muted controls className={style.video}>
-                <source src={kalingaVideo} type="video/mp4" />
-                Your browser does not support the video tag.
-                </video>
-            </div>
-            
-            
-                
-                
-            </div>
-        </>
-    )
+        <div className={style.video_container}>
+          <video autoPlay loop muted controls className={style.video}>
+            <source src={kalingaVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Body_3;
