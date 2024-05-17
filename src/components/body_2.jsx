113cf4e4d25c4d76e4c4b31ca0f4adefcc03e86e
Ayush-Sharma-2023 <2022007449.ayush@ug.sharda.ac.in>
2024-05-17 17:40:11 +0530

    modified:   src/components/body_2.jsx 	modified:   src/components/body_2.module.css

--- src/components/body_2.jsx
+++ src/components/body_2.jsx
@@ -1,4 +1,17 @@
 import style from "./body_2.module.css";
+import { RiNurseLine } from "react-icons/ri";
+import { RiMicroscopeLine } from "react-icons/ri";
+import { RiTodoLine } from "react-icons/ri";
+import { MdOutlineFolderCopy } from "react-icons/md";
+import { RiTwitchLine } from "react-icons/ri";
+import { RiGlobalLine } from "react-icons/ri";
+
+
+
+
+
+
+
 
 function Body_2() {
   return (
@@ -12,7 +25,8 @@ function Body_2() {
 
         <div className={style.Features_Container}>
           <div className={style.box}>
-            <div className={style.title}> For Students</div>
+            {/* <div className={style.logo}><RiNurseLine /></div> */}
+            <div className={style.title}><RiNurseLine /> For Students</div>
             <div className={style.info}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
               libero atque rem commodi, pariatur voluptatum.
@@ -20,7 +34,7 @@ function Body_2() {
           </div>
 
           <div className={style.box}>
-            <div className={style.title}>For Researchers</div>
+            <div className={style.title}> <RiMicroscopeLine /> For Researchers</div>
             <div className={style.info}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
               libero atque rem commodi, pariatur voluptatum.
@@ -28,7 +42,7 @@ function Body_2() {
           </div>
 
           <div className={style.box}>
-            <div className={style.title}>For Professionals</div>
+            <div className={style.title}> <RiTodoLine /> For Professionals</div>
             <div className={style.info}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
               libero atque rem commodi, pariatur voluptatum.
@@ -36,7 +50,7 @@ function Body_2() {
           </div>
 
           <div className={style.box}>
-            <div className={style.title}>Multi-File Chats</div>
+            <div className={style.title}> <MdOutlineFolderCopy /> Multi-File Chats</div>
             <div className={style.info}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
               libero atque rem commodi, pariatur voluptatum.
@@ -44,7 +58,7 @@ function Body_2() {
           </div>
 
           <div className={style.box}>
-            <div className={style.title}>Cited Sources</div>
+            <div className={style.title}> <RiTwitchLine /> Cited Sources</div>
             <div className={style.info}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
               libero atque rem commodi, pariatur voluptatum.
@@ -52,7 +66,7 @@ function Body_2() {
           </div>
 
           <div className={style.box}>
-            <div className={style.title}>Any Language</div>
+            <div className={style.title}><RiGlobalLine /> Any Language</div>
             <div className={style.info}>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea
               libero atque rem commodi, pariatur voluptatum.
