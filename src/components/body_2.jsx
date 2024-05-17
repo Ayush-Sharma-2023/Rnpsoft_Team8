1b9e8a0044da179309cdb8f38f1b6819aa47132c
Ayush-Sharma-2023 <2022007449.ayush@ug.sharda.ac.in>
2024-05-17 12:06:52 +0530

    modified:   src/components/body_2.jsx 	modified:   src/components/body_2.module.css

--- src/components/body_2.jsx
+++ src/components/body_2.jsx
@@ -9,6 +9,44 @@ function Body_2() {
             
                 
                 <div className={style.Features}>Features</div>
+                <br/><br/><br/><br/>
+
+                <div className={style.Features_Container}>
+
+                    <div className={style.box}>
+                        <div className={style.title}> For Students</div>
+                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
+                    </div>
+
+                    <div className={style.box}>
+                        <div className={style.title}>For Researchers</div>
+                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
+                    </div>
+
+                    <div className={style.box}>
+                        <div className={style.title}>For Professionals</div>
+                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
+                    </div>
+                    
+
+                    <div className={style.box}>
+                        <div className={style.title}>Multi-File Chats</div>
+                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
+                    </div>
+
+                    <div className={style.box}>
+                        <div className={style.title}>Cited Sources</div>
+                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
+                    </div>
+
+                    <div className={style.box}>
+                        <div className={style.title}>Any Language</div>
+                        <div className={style.info}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea libero atque rem commodi, pariatur voluptatum.</div>
+                    </div>
+                
+                
+
+                </div>
 
                 
                 
