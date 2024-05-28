21359fd308be0cd12d22283726bb644b883fafd3
bhavya <bhavya.gabaa@gmail.com>
2024-05-28 19:20:39 +0530

    made changes in contact us and tested it.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Body_3 from './components/msAiAnchor.jsx'
 import Body_4 from './components/body_4.jsx'
 import Body_5 from './components/body_5.jsx'
 import Footer from './components/footer.jsx'
-import Contact from "./components/contact-us.jsx"
+import Contact from "./components/contactus.jsx"
 
 
 
@@ -25,7 +25,7 @@ function App() {
     <Multilingual />
     {/* <Body_4 /> */}
     {/* <Body_5 /> */}
-    {/* <Contact /> */}
+    <Contact />
     <Footer />
 
   </>
