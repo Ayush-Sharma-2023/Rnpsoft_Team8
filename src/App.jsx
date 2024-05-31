d41ff96774d3fa446c833bfa3d072164642336a5
Ayush-Sharma-2023 <2022007449.ayush@ug.sharda.ac.in>
2024-06-01 00:14:22 +0530

    scroll effect from contact page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Navbar from "./components/navbar.jsx"
 import Body_1 from "./components/body_1.jsx"
@@ -19,17 +17,52 @@ import React, { useRef } from 'react';
 function App() {
 
   const [showContact, setShowContact] = useState(false);
+  const [scrollTarget, setScrollTarget] = useState(null);
 
  
  const handleContactClick = () => {
   setShowContact(true);
 };
 
+const showHome = () => {
+  setShowContact(false);
+};
+
 
 const handleHomeClick = () => {
-  setShowContact(false);
-  homeRef.current.scrollIntoView({ behavior: 'smooth' });
+  showHome();  
+  setScrollTarget(homeRef);
 };
+
+// 
+// Handle demo click
+const handleDemoClick = () => {
+  setShowContact(false); 
+  setScrollTarget(demoRef);
+};
+
+const handleonAiClick = () => {
+  setShowContact(false); 
+  // setScrollTarget(whyAIRef);
+  setScrollTarget(featuresRef);
+};
+
+const handleonFeaClick = () => {
+  setShowContact(false); 
+  setScrollTarget(featuresRef);
+};
+//
+
+
+useEffect(() => {
+  if (!showContact && scrollTarget) {
+    scrollTarget.current.scrollIntoView({ behavior: 'smooth' }); // Change to 'smooth' if needed
+    setScrollTarget(null); // Clear the scroll target after scrolling
+  }
+}, [showContact, scrollTarget]);
+
+
+
 
   const homeRef = useRef(null);
   const whyAIRef = useRef(null);
@@ -42,12 +75,18 @@ const handleHomeClick = () => {
     <Navbar 
     onContactClick={handleContactClick}
     onHomeClick={handleHomeClick}
+    onDemoClick={handleDemoClick}
 
+    onAiClick={handleonAiClick}
+    onFeaClick={handleonFeaClick}
+    
     homeRef={homeRef} 
     whyAIRef={whyAIRef} 
     demoRef={demoRef} 
     featuresRef={featuresRef} 
      />
+
+
     {showContact ? (
       <Contact />
     ) : (
