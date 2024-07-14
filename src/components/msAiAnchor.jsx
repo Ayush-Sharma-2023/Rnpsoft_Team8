dbd93e5fafeea83e096f75f0fbfc5ba58818cd30
Ayush-Sharma-2023 <Ayushsharmaarps@gmail.com>
2024-07-14 11:32:43 +0530

    Worked on the Multilingual section 	modified:   src/components/msAiAnchor.jsx 	modified:   src/components/msAiAnchor.module.css 	new file:   src/images/language.jpg

--- src/components/msAiAnchor.jsx
+++ src/components/msAiAnchor.jsx
@@ -1,5 +1,6 @@
 import style from "./msAiAnchor.module.css";
 import kalingaVideo from "../images/kalinga_ai.mp4";
+import language from "../images/language.jpg"
 
 
 function msAiAnchor() {
@@ -10,25 +11,35 @@ function msAiAnchor() {
       <div className={style.backgr}>
         <br />
         <br />
-        <div className={style.Demo}>Meet Ms. Kalinga : Your  AI News Anchor</div>
+        <div className="Texts">
+
+        <div className={style.title}>Ms <span>Kalinga</span></div>
+        <div className={style.subtitle}>IS MULTILINGUAL</div>
         <br />
         <div className={style.info}>
           
-"Experience the future of news delivery with Ms. Kalinga, our state-of-the-art AI news anchor. From real-<br/>
-time updates to multilingual reporting, Ms. Kalinga brings cutting-edge technology to your newsroom,<br/> providing accurate and engaging news coverage tailored to your audience. Explore her dynamic<br/> features and see how she can revolutionize the way you broadcast."
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
 
           
         </div>
-        <br />
-        <br />
 
-        <div className={style.video_container}>
+        </div>
+        <div className={style.languagePic}>
+          <p className={style.languageInfo} >Press here to hear her in your own language</p>
+          
+        {/* <img className={style.language} src={language} alt="" /> */}
+        </div>
+
+        {/* <br />
+        <br /> */}
+
+        {/* <div className={style.video_container}>
           <video autoPlay loop muted controls className={style.video}>
             <source src={kalingaVideo} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           
-        </div>
+        </div> */}
 
         
       </div>
