45ec0660ef2f011b99a30f3eacb3b2a08596f867
Ayush-Sharma-2023 <Ayushsharmaarps@gmail.com>
2024-07-17 10:41:20 +0530

    modified:   src/components/msAiAnchor.jsx 	modified:   src/components/msAiAnchor.module.css 	modified:   src/components/multilingual.jsx

--- src/components/msAiAnchor.jsx
+++ src/components/msAiAnchor.jsx
@@ -1,90 +1,72 @@
 import style from "./msAiAnchor.module.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import kalingaVideo from "../images/kalinga_ai.mp4";
-import language from "../images/language.jpg"
-
+import language from "../images/language.jpg";
 
 function msAiAnchor() {
-
-  const [activeSection, setActiveSection] = useState('section1');
+  const [activeSection, setActiveSection] = useState("section1");
 
   const handleClick = () => {
-    
-    setActiveSection('section2');
+    setActiveSection("section2");
   };
 
-
   return (
-
     <>
-          {activeSection == 'section1' ? (
-                 /*This section is the default section i.e "MS KALINGA IS MULTILINGUAL"  */
-                 <div className={style.backgr}>
-               <br />
-               <br />
-               <div className="Texts">
-       
-               <div className={style.title}>Ms <span>Kalinga</span></div>
-               <div className={style.subtitle}>IS MULTILINGUAL</div>
-               <br />
-               <div className={style.info}>
-                 
-       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
-       
-                 
-               </div>
-       
-               </div>
-               <div onClick={handleClick} className={style.languagePic}>
-                 <p className={style.languageInfo} >Press here to hear her in your own language</p>
-                 
-               
-               </div>
-       
-              
-       
-               {/* <div className={style.video_container}>
+      {activeSection == "section1" ? (
+        /*This section is the default section i.e "MS KALINGA IS MULTILINGUAL"  */
+        <div className={style.backgr}>
+          <br />
+          <br />
+          <div className="Texts">
+            <div className={style.title}>
+              Ms <span>Kalinga</span>
+            </div>
+            <div className={style.subtitle}>IS MULTILINGUAL</div>
+            <br />
+            <div className={style.info}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex
+            </div>
+          </div>
+          <div onClick={handleClick} className={style.languagePic}>
+            {" "}
+            {/* This is the Pic, the language pic, its properties are added using CSS */}
+            <p className={style.languageInfo}>
+              Press here to hear her in your own language
+            </p>
+          </div>
+
+          {/* <div className={style.video_container}>
                  <video autoPlay loop muted controls className={style.video}>
                  <source src={kalingaVideo} type="video/mp4" />
                  Your browser does not support the video tag.
                  </video>
                  
                  </div> */}
-       
-               
-             </div>
-             /* MS KALINGA IS MULTILIGUAL SECTION ENDS */
-             
-             
-):(
-  
-  <>
-  {/* THIS SECTION EXECUTES IF THE PICTURE IS PRESSED i.e THE CHOOSE LANGUAGE PART*/}
+        </div>
+      ) : (
+        /* MS KALINGA IS MULTILIGUAL SECTION ENDS */
 
-    <div className={style.langBox}>
-    
-  <div className={style.languageSelect}>
+        <>
+          {/* THIS SECTION EXECUTES IF THE PICTURE IS PRESSED i.e THE CHOOSE LANGUAGE PART*/}
 
+          <div className={style.langBox}>
+            <div className={style.languageSelect}></div> {/*This is the language pic in the language select section,Its propertites are added using CSS */}
+            <div className={style.video_container}>
+              <p className={style.lang}>MS KALINA IN HINDI</p> <br />
+              <br />
+              <br />
+              <video autoPlay loop muted controls className={style.video}>
+                <source src={kalingaVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
           </div>
-
-               <div className={style.video_container}>
-                <p className={style.lang}>MS KALINA IN HINDI</p> <br /><br /><br />
-                <video autoPlay loop muted controls className={style.video}>
-                  <source src={kalingaVideo} type="video/mp4" />
-                  Your browser does not support the video tag.
-                </video>
-                
-              </div> 
-            
-              </div>
-
-
-          </>
-          )}
-
-    
-   
+        </>
+      )}
     </>
   );
 }
--- src/components/multilingual.jsx
+++ src/components/multilingual.jsx
@@ -1,77 +1,56 @@
 import style from "./multilingual.module.css";
 import kalingaVideo from "../images/kalinga_ai.mp4";
-import Marquee from "react-fast-marquee"
+import Marquee from "react-fast-marquee";
 function multilingual() {
   return (
     <>
       <div className={style.backgr}>
-
         <div className={style.up_container}>
-
           <div className={style.solar}>
-            <div className={style.vector}>
-
-            </div>
+            <div className={style.vector}></div>
 
             <div className={style.orbit}>
-                <div className={style.logo}
-                ></div>
-
+              <div className={style.logo}></div>
             </div>
 
-            
             <div className={style.orbit2}>
-                <div className={style.logo2}
-                ></div>
-
+              <div className={style.logo2}></div>
             </div>
             <div className={style.orbit3}>
-                <div className={style.logo3}
-                ></div>
-
+              <div className={style.logo3}></div>
             </div>
             <div className={style.orbit4}>
-                <div className={style.logo4}
-                ></div>
-
+              <div className={style.logo4}></div>
             </div>
             <div className={style.orbit5}>
-                <div className={style.logo5}
-                ></div>
-
+              <div className={style.logo5}></div>
             </div>
           </div>
-          
-          <div className={style.container}>7
-            <h1>Features</h1>
 
+          <div className={style.container}>
+            7<h1>Features</h1>
             <p>
               You will be shocked to know the fact that Ms Kalinga can
               <br /> speak Hindi, Odia and English pretty well.
             </p>
-
             <br />
-
             <p>
               You will be shocked to know the fact that Ms Kalinga can
               <br /> speak Hindi, Odia and English pretty well. You will be
               <br /> shocked to know the fact that Ms Kalinga can speak Hindi,
               <br /> Odia and English pretty well.
             </p>
-
           </div>
 
-          <br /><br /><br />
+          <br />
+          <br />
+          <br />
         </div>
 
         {/* <div className="marquee"> */}
 
-          <Marquee play speed={300} pauseOnHover>
-          
-
-          
+        <Marquee play speed={300} pauseOnHover>
           <div className={style.featuresContainer}>
-
             <div className={style.feature}>
               <div className={style.text}>Feature 1</div>
             </div>
@@ -79,7 +58,7 @@ function multilingual() {
             <div className={style.feature}>
               <div className={style.text}>Feature 2</div>
             </div>
-            
+
             <div className={style.feature}>
               <div className={style.text}>Feature 3</div>
             </div>
@@ -87,17 +66,10 @@ function multilingual() {
             <div className={style.feature}>
               <div className={style.text}>Feature 4</div>
             </div>
-       
-
-
           </div>
-
-          </Marquee>
-
-
+        </Marquee>
 
         {/* </div> */}
-
       </div>
     </>
   );
